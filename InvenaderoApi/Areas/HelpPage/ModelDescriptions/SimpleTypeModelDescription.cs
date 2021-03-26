307ddd275ce39491b22b4db89d22e1e3ae16a690
daylighted <45243229+daylighted@users.noreply.github.com>
2021-03-25 22:32:24 -0600

    Prueba 1
    Ups

--- InvenaderoApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace InvenaderoApi.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- InvenaderoApi/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="InvenaderoApi.WebApiApplication" Language="C#" %>
