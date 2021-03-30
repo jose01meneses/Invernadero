790c5cc2ad72d5aef3f04ef0fb5c63ebdeb08168
Brayanbs99 <bryanbs2399@gmail.com>
2021-03-30 10:39:37 -0600

    Prueba Brayan Desktop

---
+++ Invernadero/Controllers/PruebaBrayanDesktopController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Invernadero.Controllers
+{
+    public class PruebaBrayanDesktopController : Controller
+    {
+        // GET: PruebaBrayanDesktop
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
